93926dec1fa0ea34625ea2330d9413d1d79ad0c6
DanielAnthes <daniel.anthes97@gmail.com>
2017-12-24 13:27:36 +0100

    initial commit

---
+++ code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 24 13:22:38 2017
+
+@author: daniel
+"""
+#%%
+
+"""
+Necessary imports
+"""
+import pandas as pa
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
+
+data = pa.read_csv('Data/mushrooms.csv')
