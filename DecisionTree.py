7b35433b21e3c9c910b5e09d98cb79e2b5fc43ed
Melle Starke <mellestarke@gmail.com>
2017-12-28 14:09:52 +0100

    coded first parts of adaboost algorithm

--- DecisionTree.py
+++ DecisionTree.py
@@ -11,125 +11,125 @@ Takes data X as numpy array and y as list or array, currently the only purity me
 
 """
 
-#%%
+# %%
 from collections import Counter
 import numpy as np
 
+
 class DecisionTree:
-    def __init__(self,purityMeasure='gini'):
+    def __init__(self, purityMeasure='gini'):
         self.purity_measure = purityMeasure
         self.depth = 1
         self.root = Node()
-        
-    def classify(self,y)  :
-        #TODO account for empty leaf nodes
+
+    def classify(self, y):
+        # TODO account for empty leaf nodes
         counter = Counter(y)
         return counter.most_common(1)[0][0]
-    
-    def generateSplit(self,X,y):
-        #Splits will always be binary, for each attribute try all possible splits and return rule of the best one
-        #TODO improve by introducing early stopping
-        #TODO improve by returning split indices
+
+    def generateSplit(self, X, y):
+        # Splits will always be binary, for each attribute try all possible splits and return rule of the best one
+        # TODO improve by introducing early stopping
+        # TODO improve by returning split indices
         num_entries, num_attr = X.shape
-        best_split = (None, None,999) #holds column, value to be split on and current lowest Gini
-        #loop over all attributes
-        for i in range(0,num_attr):
-            currentBest = (None,None,1) #holds column, value to be split on and gini
-            attr = X[:,i] #single out relevant column
-            unique = np.unique(attr) # get unique values of attribute, split on each
+        best_split = (None, None, 999)  # holds column, value to be split on and current lowest Gini
+        # loop over all attributes
+        for i in range(0, num_attr):
+            currentBest = (None, None, 1)  # holds column, value to be split on and gini
+            attr = X[:, i]  # single out relevant column
+            unique = np.unique(attr)  # get unique values of attribute, split on each
             for u in unique:
-                #get indices on condition
-                left_indices =  list()
+                # get indices on condition
+                left_indices = list()
                 right_indices = list()
                 for j in range(0, num_entries):
                     if attr[j] == u:
                         left_indices.append(j)
                     else:
                         right_indices.append(j)
-                #use indices to split y
+                # use indices to split y
                 y1 = y[left_indices]
                 y2 = y[right_indices]
-                #compute gini
-                gini = (len(left_indices)/float(num_entries)) * self.gini(y1) + (len(right_indices)/float(num_entries)) * self.gini(y2) 
+                # compute gini
+                gini = (len(left_indices) / float(num_entries)) * self.gini(y1) + (
+                        len(right_indices) / float(num_entries)) * self.gini(y2)
                 if currentBest[2] > gini:
-                    #update best condition
-                    currentBest = (i,u,gini)
-            #update overall best split
+                    # update best condition
+                    currentBest = (i, u, gini)
+            # update overall best split
             if best_split[2] > currentBest[2]:
                 best_split = currentBest
-        
-        
-        return (best_split[0],best_split[1]) #returns column and value to be split on
-        
-    def gini(self,y):
+
+        return best_split[0], best_split[1]  # returns column and value to be split on
+
+    def gini(self, y):
         length = float(len(y))
-        unique,counts = np.unique(y, return_counts = True)
+        unique, counts = np.unique(y, return_counts=True)
         helper = 0
-        for i in range(0,len(counts)):
-            helper += (counts[i]/length)**2
+        for i in range(0, len(counts)):
+            helper += (counts[i] / length) ** 2
         return 1 - helper
-            
-        
-    def stop(self, X, y, max_depth = None, minNodeSize = None):
-        #also stop if there is no more gain?
+
+    def stop(self, X, y, max_depth=None, minNodeSize=None):
+        # also stop if there is no more gain?
         size = len(X)
         if self.gini(y) == 0:
             return True
         if self.depth >= max_depth:
             return True
-        
+
         elif size < minNodeSize:
             return True
-        
+
         elif size < 2:
             return True
-        
+
         else:
             return False
-        
-    def fitTree(self,X,y, currNode = None,max_depth = None,minNodeSize=None):
-        #TODO does not work properly without setting a max_depth
-        #TODO Nodes are not added correctly!
-        #in first call get root Node
+
+    def fitTree(self, X, y, currNode=None, max_depth=None, minNodeSize=None):
+        # TODO does not work properly without setting a max_depth
+        # TODO Nodes are not added correctly!
+        # in first call get root Node
         if currNode == None:
             newNode = self.root
         else:
             newNode = currNode
-        if self.stop(X = X, y = y, max_depth = max_depth, minNodeSize = minNodeSize):
-            #create leaf node, assign classification
+        if self.stop(X=X, y=y, max_depth=max_depth, minNodeSize=minNodeSize):
+            # create leaf node, assign classification
             leaf = newNode
             leaf.classification = self.classify(y)
-            
+
         else:
-            #create node, generate test condition
+            # create node, generate test condition
             self.depth += 1
             node = newNode
-            node.splitCriteria = self.generateSplit(X,y)
-            indices_left  = list()
+            node.splitCriteria = self.generateSplit(X, y)
+            indices_left = list()
             indices_right = list()
-            
-            #generate binary split on condition: True is left child, False is right child
-            for i in range(0,len(X)):
+
+            # generate binary split on condition: True is left child, False is right child
+            for i in range(0, len(X)):
                 element = X[i]
                 if node.condition(element):
                     indices_left.append(i)
                 else:
                     indices_right.append(i)
-            #recursively call fitTree for child nodes
-            node.left  = Node()
-            self.fitTree( X[indices_left], y[indices_left], node.left, max_depth, minNodeSize)
+            # recursively call fitTree for child nodes
+            node.left = Node()
+            self.fitTree(X[indices_left], y[indices_left], node.left, max_depth, minNodeSize)
             node.right = Node()
             self.fitTree(X[indices_right], y[indices_right], node.right, max_depth, minNodeSize)
-    
+
     def predict(self, element):
         if self.root == None:
             print("call fitTree first to generate tree")
             return None
-        #add support for classification of multiple elements at once
+        # add support for classification of multiple elements at once
         elif np.ndim(element) > 1:
             length = len(element)
             results = np.empty(length)
-            for i in range(0,length):
+            for i in range(0, length):
                 e = element[i]
                 results[i] = self.predict(e)
             return results
@@ -143,18 +143,18 @@ class DecisionTree:
                 else:
                     current = current.right
             return current.classification
-            
-            
-#Define Node objects for DecisionTree            
+
+
+# Define Node objects for DecisionTree
 class Node():
     def __init__(self):
         self.classification = None
-        self.splitCriteria = None #column and value to split on
+        self.splitCriteria = None  # column and value to split on
         self.left = None
         self.right = None
-    
-    def condition(self,X):
-        #takes as input one datapoint
+
+    def condition(self, X):
+        # takes as input one datapoint
         if self.splitCriteria == None:
             print("no split Criteria set")
             return None
@@ -162,5 +162,3 @@ class Node():
             col = self.splitCriteria[0]
             con = self.splitCriteria[1]
             return X[col] == con
-        
-    
--- testCode.py
+++ testCode.py
@@ -5,7 +5,7 @@ Created on Sun Dec 24 13:22:38 2017
 
 @author: daniel
 """
-#%%
+# %%
 
 """
 Necessary imports
@@ -15,28 +15,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import DecisionTree as dt
 
-#%%
+# %%
 
 data = pa.read_csv('Data/mushrooms.csv')
-y = data['class']                   #class
-X = data.iloc[:,range(1,23)]        #attributes
+y = data['class']  # class
+X = data.iloc[:, range(1, 23)]  # attributes
 
 attributes = X.columns.values
 X_arr = X.values
 y_arr = y.values
 
-#%% test data
+# %% test data
 
-testX = np.array([[0,0],[0,0],[0,0],[0,0],[0,1],[0,1],[0,1],[0,1],[1,0],[1,0],[1,0],[1,0],[1,1],[1,1],[1,1],[1,1]])
-testy = np.array([0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3])
+testX = np.array(
+        [[0, 0], [0, 0], [0, 0], [0, 0], [0, 1], [0, 1], [0, 1], [0, 1], [1, 0], [1, 0], [1, 0], [1, 0], [1, 1], [1, 1],
+         [1, 1], [1, 1]])
+testy = np.array([0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3])
 
-#%%
+# %%
 
 tree = dt.DecisionTree()
-tree.fitTree(X = testX, y = testy, max_depth = 100)
-tree.depth
+tree.fitTree(X=testX, y=testy, max_depth=100)
 
-#%%
-#testing
-print(tree.predict(np.array([[0,0],[0,1]])))
-
+# %%
+# testing
+print(tree.predict(np.array([[0, 0], [0, 1]])))
