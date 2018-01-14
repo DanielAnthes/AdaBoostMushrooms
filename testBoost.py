de673b2e318abf50bcef8d9e3b90108ed6f81c04
DanielAnthes <daniel.anthes97@gmail.com>
2018-01-14 19:33:35 +0100

    add work on test file

--- testBoost.py
+++ testBoost.py
@@ -7,6 +7,7 @@ import AdaBoost as ab
 import numpy as np
 import pandas as pa
 import DecisionTree as dt
+from collections import Counter
 
 boost = ab.AdaBoost()
 X_arr = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1]])
@@ -33,19 +34,38 @@ y_test = y_arr[test_indices]
 
 
 #boost.train(X,y)
-boost.train(X_train,y_train,numClassifiers=10)
+boost.train(X_train,y_train,numClassifiers=20)
 
 #now test with remaining data
 pred_boost = boost.predict(X_test)
 error_rate_boost = (sum([0 if pred == true else 1 for (pred, true) in zip(y_test, pred_boost)]) / float(len(y_test)))
-print('Adaboost error rate: ',error_rate_boost)
-
 
 #now for decision tree
 tree = dt.DecisionTree()
 tree.fitTree(X_train,y_train)
 pred_tree = tree.predict(X_test)
 error_rate_tree = (sum([0 if pred == true else 1 for (pred, true) in zip(y_test, pred_tree)]) / float(len(y_test)))
+
+'''
+Add prints for diagnostics and results here:
+'''
+print('')
+print('***** RESULTS ADABOOST *****')
+decisions = [c.root.splitCriteria for c in boost.classifiers]
+print('Decision Criteria per trees used: ', decisions)
+decision_columns = [c for c,_ in decisions]
+unique_cols = np.unique(decision_columns)
+col_counts = Counter(decision_columns).most_common()
+print('unique columns and frequency of split columns: ', col_counts)
+
+
+print('Adaboost error rate: ',error_rate_boost)
+
+
+
+
+print('')
+print('***** RESULTS DECISION TREE *****')
 print('DecisionTree error rate: ', error_rate_tree)
 print("done")
 
